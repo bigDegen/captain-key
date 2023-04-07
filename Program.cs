3937e7b95a7633d7fe160be5bd75da999ad86756
Samuel <87152786+bigDegen@users.noreply.github.com>
2023-04-07 05:35:51 +0200

    inital

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// bigdegen 2023
+
+using System;
+using MyApp.Util;
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("The output will be in the root folder of CaptainKey by default");
+            
+            Capture logger = new Capture(@"./log.txt");
+            Console.ReadLine();
+            logger.StopLogging();
+        }
+        
+    }
+}
